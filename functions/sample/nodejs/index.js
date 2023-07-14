b7afe8af5de82586b4280efcc456187e816d3149
AndradeEduardo <dudsdu@gmail.com>
2023-07-14 02:49:28 -0400

    updating functions codes with key - I am tired of these time underestimated assignments

--- functions/sample/nodejs/index.js
+++ functions/sample/nodejs/index.js
@@ -1,30 +1,70 @@
-/**
- * Get all databases
- */
-
- const { CloudantV1 } = require("@ibm-cloud/cloudant");
- const { IamAuthenticator } = require("ibm-cloud-sdk-core");
- 
- function main(params) {
-   const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY });
-   const cloudant = CloudantV1.newInstance({
-     authenticator: authenticator,
-   });
-   cloudant.setServiceUrl(params.COUCH_URL);
- 
-   let dbList = getDbs(cloudant);
-   return { dbs: dbList };
- }
- 
- function getDbs(cloudant) {
-   cloudant
-     .getAllDbs()
-     .then((body) => {
-       body.forEach((db) => {
-         dbList.push(db);
-       });
-     })
-     .catch((err) => {
-       console.log(err);
-     });
- }
+function main(params) {
+    // console.log(params);
+    return new Promise(function (resolve, reject) {
+        const { CloudantV1 } = require('@ibm-cloud/cloudant');
+        const { IamAuthenticator } = require('ibm-cloud-sdk-core');
+        const authenticator = new IamAuthenticator({ apikey: 'HmeG7P8FwEkUYNYPxNyYSww5uzGd4cI4PkBKlor-GIE7' })
+        const cloudant = CloudantV1.newInstance({
+            authenticator: authenticator
+        });
+        cloudant.setServiceUrl('https://5f8b49ed-9f92-4248-ba7d-af4618075e6d-bluemix.cloudantnosqldb.appdomain.cloud');
+        if (params.st) {
+            // return dealership with this state 
+            cloudant.postFind({db:'dealerships',selector:{st:params.st}})
+            .then((result)=>{
+              // console.log(result.result.docs);
+              let code = 200;
+              if (result.result.docs.length == 0) {
+                  code = 404;
+              }
+              resolve({
+                  statusCode: code,
+                  headers: { 'Content-Type': 'application/json' },
+                  body: result.result.docs
+              });
+            }).catch((err)=>{
+              reject(err);
+            })
+        } else if (params.id) {
+            id = parseInt(params.dealerId)
+            // return dealership with this state 
+            cloudant.postFind({
+              db: 'dealerships',
+              selector: {
+                id: parseInt(params.id)
+              }
+            })
+            .then((result)=>{
+              // console.log(result.result.docs);
+              let code = 200;
+              if (result.result.docs.length == 0) {
+                  code = 404;
+              }
+              resolve({
+                  statusCode: code,
+                  headers: { 'Content-Type': 'application/json' },
+                  body: result.result.docs
+              });
+            }).catch((err)=>{
+              reject(err);
+            })
+        } else {
+            // return all documents 
+            cloudant.postAllDocs({ db: 'dealerships', includeDocs: true, limit: 10 })            
+            .then((result)=>{
+              // console.log(result.result.rows);
+              let code = 200;
+              if (result.result.rows.length == 0) {
+                  code = 404;
+              }
+              resolve({
+                  statusCode: code,
+                  headers: { 'Content-Type': 'application/json' },
+                  body: result.result.rows
+              });
+            }).catch((err)=>{
+              reject(err);
+            })
+      }
+    }
+    )}
--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -1,35 +1,24 @@
-"""IBM Cloud Function that gets all reviews for a dealership
+import sys 
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-Returns:
-    List: List of reviews for the given dealership
-"""
-from cloudant.client import Cloudant
-from cloudant.error import CloudantException
-import requests
-
-
-def main(param_dict):
-    """Main Function
-
-    Args:
-        param_dict (Dict): input paramater
-
-    Returns:
-        _type_: _description_ TODO
-    """
-
-    try:
-        client = Cloudant.iam(
-            account_name=param_dict["COUCH_USERNAME"],
-            api_key=param_dict["IAM_API_KEY"],
-            connect=True,
-        )
-        print(f"Databases: {client.all_dbs()}")
-    except CloudantException as cloudant_exception:
-        print("unable to connect")
-        return {"error": cloudant_exception}
-    except (requests.exceptions.RequestException, ConnectionResetError) as err:
-        print("connection error")
-        return {"error": err}
-
-    return {"dbs": client.all_dbs()}
+def main(dict): 
+    authenticator = IAMAuthenticator("HmeG7P8FwEkUYNYPxNyYSww5uzGd4cI4PkBKlor-GIE7")
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url("https://5f8b49ed-9f92-4248-ba7d-af4618075e6d-bluemix.cloudantnosqldb.appdomain.cloud")
+    response = service.post_find(
+                db='reviews',
+                selector={'dealership': {'$eq': int(dict['id'])}},
+            ).get_result()
+    try: 
+        # result_by_filter=my_database.get_query_result(selector,raw_result=True) 
+        result= {
+            'headers': {'Content-Type':'application/json'}, 
+            'body': {'data':response} 
+            }        
+        return result
+    except:  
+        return { 
+            'statusCode': 404, 
+            'message': 'Something went wrong'
+            }
