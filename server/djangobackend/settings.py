9d5ce9b2cb3e2ad1a3961fbac7ee733a7a7e10ce
AndradeEduardo <dudsdu@gmail.com>
2023-07-18 21:18:28 -0400

    Posts not working

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 APPEND_SLASH = True
 
 ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ['localhost', 'localhost:8000',  'https://dudsdu-8000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']  
 
 
 # Application definition
